171138d0c8b1cfe104b3d3b3bae1bd27a882c6f1
He Zhang <hxzhang1@ualr.edu>
2018-01-30 23:24:07 -0600

    Succeed to compile and run this project

--- CarND-MPC-Quizzes/mpc_to_line/src/MPC.cpp
+++ CarND-MPC-Quizzes/mpc_to_line/src/MPC.cpp
@@ -11,8 +11,8 @@ namespace plt = matplotlibcpp;
 using CppAD::AD;
 
 // TODO: Set N and dt
-size_t N = ? ;
-double dt = ? ;
+size_t N = 25 ;
+double dt = 0.05 ;
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -59,6 +59,26 @@ class FG_eval {
     // Reference State Cost
     // TODO: Define the cost related the reference state and
     // any anything you think may be beneficial.
+    // difference from the reference state 
+    for(int i=0; i<N; i++)
+    {
+	fg[0] += CppAD::pow(vars[cte_start+i],2 ); 
+	fg[0] += CppAD::pow(vars[epsi_start+i], 2); 
+	fg[0] += CppAD::pow(vars[v_start+i] - ref_v, 2); 
+    }
+	
+    // minimize the use of actuators
+    for(int i=0; i<N-1; i++)
+    {
+	fg[0] += CppAD::pow(vars[delta_start+i], 2);
+	fg[0] += CppAD::pow(vars[a_start+i], 2); 
+    }
+    // minimize the value gap between sequential actuations
+    for(int i=0; i<N-2; i++)
+    {
+	fg[0] += CppAD::pow(vars[a_start+i+1] - vars[a_start+i], 2); 
+	fg[0] += CppAD::pow(vars[delta_start+i+1] - vars[delta_start+i], 2);
+    }
 
     //
     // Setup Constraints
@@ -78,12 +98,30 @@ class FG_eval {
     fg[1 + epsi_start] = vars[epsi_start];
 
     // The rest of the constraints
-    for (int t = 1; t < N; t++) {
+    for (int t = 1; t < N; t++) 
+    {
+	// state at time t+1
       AD<double> x1 = vars[x_start + t];
+      AD<double> y1 = vars[y_start + t]; 
+      AD<double> psi1 = vars[psi_start + t]; 
+      AD<double> v1 = vars[v_start + t]; 
+      AD<double> cte1 = vars[cte_start + t];
+      AD<double> epsi1 = vars[epsi_start + t]; 
 
+	// state at time t
       AD<double> x0 = vars[x_start + t - 1];
+      AD<double> y0 = vars[y_start + t - 1];
       AD<double> psi0 = vars[psi_start + t - 1];
       AD<double> v0 = vars[v_start + t - 1];
+      AD<double> cte0 = vars[cte_start + t - 1];
+      AD<double> epsi0 = vars[epsi_start + t - 1]; 
+	
+	// consider the actuation at time t
+      AD<double> delta0 = vars[delta_start + t -1]; 
+      AD<double> a0 = vars[a_start + t - 1]; 
+      
+      AD<double> f0 = coeffs[0] + coeffs[1] * x0; 
+      AD<double> psides0 = CppAD::atan(coeffs[1]); 
 
       // Here's `x` to get you started.
       // The idea here is to constraint this value to be 0.
@@ -94,6 +132,11 @@ class FG_eval {
 
       // TODO: Setup the rest of the model constraints
       fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
+      fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt); 
+      fg[1 + psi_start + t] = psi1 - (psi0 + v0 * delta0 / Lf * dt); 
+      fg[1 + v_start + t] = v1 - (v0 + a0 * dt); 
+      fg[1 + cte_start + t] = cte1 - ((f0 - y0) + (v0 * CppAD::sin(epsi0) * dt));
+      fg[1 + epsi_start + t] = epsi1 - ((psi0 - psides0) + v0 * delta0 / Lf * dt);
     }
   }
 };
@@ -263,7 +306,7 @@ int main() {
   ptsy << -1, -1;
 
   // TODO: fit a polynomial to the above x and y coordinates
-  auto coeffs = ? ;
+  auto coeffs = polyfit(ptsx, ptsy, 1);
 
   // NOTE: free feel to play around with these
   double x = -1;
@@ -271,9 +314,9 @@ int main() {
   double psi = 0;
   double v = 10;
   // TODO: calculate the cross track error
-  double cte = ? ;
+  double cte = polyeval(coeffs, x) - y ;
   // TODO: calculate the orientation error
-  double epsi = ? ;
+  double epsi = psi - atan(coeffs[1]) ;
 
   Eigen::VectorXd state(6);
   state << x, y, psi, v, cte, epsi;
